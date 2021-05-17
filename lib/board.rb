b2bd0800a147c85fa8c108805a69d8f9649c9d8f
mahbubul14 <mahbub21alam2017@gmailcom>
2021-05-17 14:32:10 +0600

    Fix rubocop error from board file

--- lib/board.rb
+++ lib/board.rb
@@ -1,46 +1,50 @@
 class Game
-    attr_reader :board, :counter
-    def initialize
-        @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        @counter = 0
-    end
+  attr_reader :board, :counter
 
-    def count
-        @counter +=1
-    end
-    def add_board(number)
-        @board[number - 1] = if @counter.odd?
-                               'X'
-                             else
-                               'O'
-                             end
-    end
-    def display
-        ["+---+---+---+",
-        "| #{@board[0]} | #{@board[1]} | #{@board[2]} |",
-        "+---+---+---+",
-        "| #{@board[3]} | #{@board[4]} | #{@board[5]} |",
-        "+---+---+---+",
-        "| #{@board[6]} | #{@board[7]} | #{@board[8]} |",
-        "+---+---+---+"]
-    end 
+  def initialize
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @counter = 0
+  end
 
-    WIN_COMBINATIONS = [ 
-        [0,1,2], # top_row 
-        [3,4,5], # middle_row 
-        [6,7,8], # bottom_row 
-        [0,3,6], # left_column 
-        [1,4,7], # center_column 
-        [2,5,8], # right_column 
-        [0,4,8], # left_diagonal 
-        [6,4,2] # right_diagonal 
-    ]
-    def won?
-        WIN_COMBINATIONS.detect do |i|
-            @board[i[0]] == @board[i[1]] && @board[i[1]] == @board[i[2]]
-        end
+  def count
+    @counter += 1
+  end
+
+  def add_board(number)
+    @board[number - 1] = if @counter.odd?
+                           'X'
+                         else
+                           'O'
+                         end
+  end
+
+  def display
+    ['+---+---+---+',
+     "| #{@board[0]} | #{@board[1]} | #{@board[2]} |",
+     '+---+---+---+',
+     "| #{@board[3]} | #{@board[4]} | #{@board[5]} |",
+     '+---+---+---+',
+     "| #{@board[6]} | #{@board[7]} | #{@board[8]} |",
+     '+---+---+---+']
+  end
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2], # top_row
+    [3, 4, 5], # middle_row
+    [6, 7, 8], # bottom_row
+    [0, 3, 6], # left_column
+    [1, 4, 7], # center_column
+    [2, 5, 8], # right_column
+    [0, 4, 8], # left_diagonal
+    [6, 4, 2] # right_diagonal
+  ].freeze
+  def won?
+    WIN_COMBINATIONS.detect do |i|
+      @board[i[0]] == @board[i[1]] && @board[i[1]] == @board[i[2]]
     end
-    def tie?
-      @board.none? { |a| a.is_a?(Integer) }
-    end
+  end
+
+  def tie?
+    @board.none? { |a| a.is_a?(Integer) }
+  end
 end

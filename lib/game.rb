0a20e507b2bde49126230e4951746055210fc1e5
mahbubul14 <mahbub21alam2017@gmailcom>
2021-05-27 12:52:22 +0600

    Add one rspec rules

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,12 @@
+class Calculator
+  def add(a,b)
+    a + b
+  end
+end
+
+
 class Game
   attr_reader :board, :counter
-
   def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @counter = 0
@@ -12,20 +18,20 @@ class Game
 
   def add_board(number)
     @board[number - 1] = if @counter.odd?
-                           'X'
-                         else
-                           'O'
-                         end
+                          'X'
+                        else
+                          'O'
+                        end
   end
 
   def display
     ['+---+---+---+',
-     "| #{@board[0]} | #{@board[1]} | #{@board[2]} |",
-     '+---+---+---+',
-     "| #{@board[3]} | #{@board[4]} | #{@board[5]} |",
-     '+---+---+---+',
-     "| #{@board[6]} | #{@board[7]} | #{@board[8]} |",
-     '+---+---+---+']
+    "| #{@board[0]} | #{@board[1]} | #{@board[2]} |",
+    '+---+---+---+',
+    "| #{@board[3]} | #{@board[4]} | #{@board[5]} |",
+    '+---+---+---+',
+    "| #{@board[6]} | #{@board[7]} | #{@board[8]} |",
+    '+---+---+---+']
   end
   WIN_COMBINATIONS = [
     [0, 1, 2],
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,2 +1,30 @@
-require 'rspec'
-require_relative '.lib/game'
+require_relative '../lib/game.rb'
+
+describe Calculator do
+    describe "#add" do
+    let(:hello) {Calculator.new}
+      it "returns the sum of two numbers" do
+        calculator = Calculator.new
+        expect(calculator.add(5, 2)).to eql(7)
+      end
+    end
+  end
+
+  describe Game do
+    describe "#initialize" do
+    subject(:a) {Game.new}
+      it "returns number between 1-9" do
+      expect(a.board).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      end
+    end
+  
+  describe "#won?" do
+    # let(:wins)  {Game.new}
+    it "returns the winner" do
+      expect(subject.won?).to eq([[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]])
+    end
+  end
+
+  # = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8],[6, 4, 2]])
+
+end
